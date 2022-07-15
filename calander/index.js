b57c7217d1520dc54d132af86b3a9bf4fb3e642f
Rodrigo Munoz <rm117579@gmail.com>
2022-07-15 08:59:56 -0500

    feat: add calendar challenge

---
+++ calander/index.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+exports.getDaysInMonth = void 0;
+/**
+ * @param {int} The month number, 0 based
+ * @param {int} The year, not zero based, required to account for leap years
+ * @return {Date[]} List with date objects for each day of the month
+ */
+function getDaysInMonth(month, year) {
+    //TODO write code here
+    return [];
+}
+exports.getDaysInMonth = getDaysInMonth;
---
+++ calander/index.ts
@@ -0,0 +1,28 @@
+/*
+For this Challenge, you will write a function that returns an array of all days in a month.
+The return array should contain exactly 42 dates every time the function is called.
+
+The function must return the days in a month dynamically for any given month of any given year.
+The first index of the array MUST be sunday and the last index in the array MUST be saturday.
+To fill in the blanks you must include previous and future months in the array.
+
+They date must be in the format of :
+Sun Dec 26 1969 00:00:00 GMT-0600 (Central Standard Time)
+
+
+An application example is provided as a png file in the repo.
+*/
+
+/**
+ * @param {int} The month number, 0 based (0 = January)
+ * @param {int} The year, not zero based, required to account for leap years
+ * @return {Date[]} List with date objects for each day of the month
+ */
+export function getDaysInMonth(month: number, year: number): Date[] {
+	//TODO write code here
+	return [];
+}
+
+// To test your function, run:
+// tsc index.ts
+// node index.js
