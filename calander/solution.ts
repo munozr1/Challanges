0a6e32bdbc7987dde6ed77df1494f05b59dd94a4
Rodrigo Munoz <rm117579@gmail.com>
2022-07-23 16:22:53 -0500

    idk:

--- calander/solution.ts
+++ calander/solution.ts
@@ -6,9 +6,8 @@
 export function getDaysInMonth(month: number, year: number): any[] {
 	let date = new Date(year, month, 1);
 	let day = new Date(date.getTime() - 86400000);
-	console.log("prev day", day);
-
 	let days: Date[] = [];
+
 	while (date.getMonth() === month || date.getDay() !== 0) {
 		days.push(new Date(date));
 		date.setDate(date.getDate() + 1);
@@ -16,12 +15,8 @@ export function getDaysInMonth(month: number, year: number): any[] {
 
 	while (days[0].getDay() !== 0) {
 		days.unshift(new Date(day));
-		//subtract a day from day to get the previous day
 		day.setDate(day.getDate() - 1);
-		console.log("😡");
 	}
-	console.log("previous day: ", -1);
-
 	return days;
 }
 
